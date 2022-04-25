ec129dfa635deed753ce4416755da7d7fdfc6ad9
Tyler Fedrizzi <tyler.fedrizzi@jhuapl.edu>
2022-04-25 10:56:29 -0400

    Fix missing import in lib

--- include/ieee802_11/meta_to_csv_sink.h
+++ include/ieee802_11/meta_to_csv_sink.h
@@ -1,26 +1,5 @@
-/* -*- c++ -*- */
-/*
- * Copyright 2021 Anton Ottosson.
- *
- * This is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this software; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street,
- * Boston, MA 02110-1301, USA.
- */
-
 #ifndef INCLUDED_IEEE802_11_META_TO_CSV_SINK_H
 #define INCLUDED_IEEE802_11_META_TO_CSV_SINK_H
-
 #include <gnuradio/block.h>
 #include <ieee802_11/api.h>
 
